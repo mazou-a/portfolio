798fc77af12cc92ea627f8302ccd94278dbb5b37
mazou-a <102946352+mazou-a@users.noreply.github.com>
2025-01-31 23:24:18 -0800

    lab 4 populate home

--- global.js
+++ global.js
@@ -76,3 +76,39 @@ if (localStorage.colorScheme) {
     setColorScheme('light dark');
     select.value = 'light dark';
 }
+
+export async function fetchJSON(url) {
+    try {
+        // Fetch the JSON file from the given URL
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error(`Failed to fetch projects: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        return data; 
+
+
+    } catch (error) {
+        console.error('Error fetching or parsing JSON data:', error);
+    }
+}
+
+export function renderProjects(project, containerElement, headingLevel = 'h2') {
+    containerElement.innerHTML = '';
+    
+    project.forEach(p => {
+        const article = document.createElement('article');
+        article.innerHTML = `
+            <${headingLevel}>${p.title}</${headingLevel}>
+            <img src="${p.image}" alt="${p.title}">
+            <p>${p.description}</p>
+        `;
+        containerElement.appendChild(article);
+      });
+}
+
+export async function fetchGitHubData(username) {
+    return fetchJSON(`https://api.github.com/users/${username}`);
+  }
